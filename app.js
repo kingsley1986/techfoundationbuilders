42e27b72caaa081a88d043ace5aa12552ed2c0a1
kingsley1986 <chukwumakingsley1@gmail.com>
2021-12-23 23:44:27 +0200

    stil working on substring from js

--- app.js
+++ app.js
@@ -83,7 +83,6 @@ app.use(flash());
 
 // Global variables
 app.use(function (req, res, next) {
-  res.user = res.locals.login;
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
   res.locals.error = req.flash("error");
