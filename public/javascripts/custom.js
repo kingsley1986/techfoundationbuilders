19e466134353b4a0b4dcf39067854465ba148614
Kingsley <chukwumakingsley1@gmail.com>
2023-11-27 17:50:39 +0200

    changed views

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -175,7 +175,7 @@
   $(".scroll-to-section a[href*=\\#]:not([href=\\#])").on("click", function () {
     if (
       location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
